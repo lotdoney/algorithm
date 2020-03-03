2b2f3e194bd7814a13e63e6d68bdf833804d6d53
lotdoney <doneycow@foxmail>
2020-03-03 18:20:34 +0800

    2020227

--- algorithm/first.cpp
+++ algorithm/first.cpp
@@ -1,7 +1,152 @@
-#include "stdio.h"
+//例题一 北京邮电大学机试题进制转化
+//https://www.nowcoder.com/kaoyan/retest/1005  二进制数
+/*
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
+int main() 
+{
+	unsigned int n;
+	
+	while (scanf_s("%d",&n)!=EOF)
+	{
+		int a = 0;
+		vector<int> binary;
+		while (n!=0)
+		{
+			binary.push_back(n % 2);
+			n = n / 2;
+		}
+		for (int i=binary.size()-1;i>=0;--i)
+		{
+			cout << binary[i];
+		}
+		putchar('\n');
+		//补充：二进制转化为十进制
+		for (int i =0;i<binary.size();i++)
+		{
+		
+				a+=binary[i] * pow(2, i);
+		}
+		cout << a <<endl;
+	}
+	//使用数组的做法
+	//unsigned int n;
+	//int count=0;
+	//int saved[40] = { 0 };
+	//while (scanf_s("%d",&n)!=EOF)
+	//{
+	//	count = 0;
+	//	while(n!=0)
+	//	{
+	//			
+	//			saved[count] = n % 2;
+	//			n = n / 2;
+	//			count++;
+	//			
+	//	
+	//	}
+	//
+	//	for (int i=count-1;i>=0;i--)
+	//	{
+	//		printf_s("%d", saved[i]);
+	//	}
+	//
+	//}
+	//
+	return 0;
+}
+*/
+
+//例题2 长度为30以内的数字输入，十进制转化为二进制，再逆序排序转化为十进制 清华大学机试题
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+
+string Divide(string str, int x)  //字符串除法
+{
+		
+	int reminder = 0;//余数
+	for (unsigned int i=0;i<str.size();++i)
+	{
+		int current = reminder * 10 + str[i] - '0';
+		str[i] = current / x + '0';
+		reminder = current % x;
+	}
+	int pos = 0;
+	while (str[pos]=='0')
+	{
+		pos++;
+	}
+	return str.substr(pos);
+
+}
+
+string Multiply(string str, int x)  //字符串乘法
+{
+	int carry = 0;//进位
+	for (int i=str.size()-1;i>=0;i--)
+	{
+		int current = x * (str[i] - '0') + carry;
+		str[i] = current % 10 + '0';
+		carry = current / 10;
+	}
+	if (carry==1)
+	{
+		str = "1" + str;
+	}
+	return str;
+}
+
+string Add(string str, int x)  //字符串加法
+{
+	int carry = x;
+	for (int i=str.size()-1;i>=0;i--)
+	{
+		int current = (str[i] - '0') + carry;
+		str[i] = current % 10 + '0';
+		carry = current / 10;
+	}
+	if (carry==1)
+	{
+		str = str + "1";
+	}
+
+	return str;
+
+}
 
 int main() 
 {
+	string str;
+	
+	while (cin>>str)
+	{
+		vector <int> binary;
+		while (str.size()!=0)
+		{
+			int last = str[str.size() - 1] - '0';
+			binary.push_back(last % 2);
+			str = Divide(str, 2);
+		}
+		string answer = "0";
+		for (int i = 0; i <= binary.size()-1; i++)
+		{  //二进制转化为十进制，就是二进制数的每一位都乘以2的相应位权
+			answer = Multiply(answer, 2);
+			answer = Add(answer, binary[i]);
+			
+		}
+		cout << answer << endl;
 
-	return 0;
+	}
+	
+
 }

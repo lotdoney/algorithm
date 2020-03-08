f7a9fc4ac70c0ad90aee8cb8b0fb7bca4511944a
lotdoney <doneycow@foxmail>
2020-03-08 10:03:22 +0800

    NEW

--- algorithm/first.cpp
+++ algorithm/first.cpp
@@ -305,3 +305,214 @@ int main()
 }
 */
 
+//例题5  矩阵相乘  举个例子 2*3的与3*2的相乘
+/*
+#include<iostream>
+#include<cstdio>
+//#include <windows.h>
+using namespace std;
+
+int answer[2][2] = { 0 };
+
+void  Multiply(int x[2][3], int y[3][2]) 
+{
+	
+	for (int i=0;i<2;i++)
+	{
+		
+		for (int j=0;j<2;j++)
+		{
+			
+			for (int k=0;k<3;k++)
+			{
+				answer[i][j] += x[i][k] * y[k][j];
+			}
+		}
+	}
+
+}
+
+void Print(int  x[2][2])
+{
+	for (int i=0;i<2;i++)
+	{
+		for (int j=0;j<2;j++)
+		{
+			printf_s("%d ", x[i][j]);
+		}
+		putchar('\n');
+	}
+}
+
+int main() 
+{
+	int  x[2][3] = { 0 };
+	int  y[3][2] = { 0 };
+
+	for (int i=0;i<2;i++)
+	{
+		for (int j =0;j<3;j++)
+		{
+			scanf_s("%d", &x[i][j]);
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			scanf_s("%d", &y[i][j]);
+		}
+	}
+	Multiply(x, y);
+	Print(answer);
+	//system("pause");
+	return 0;
+}
+*/
+//拓展 快速幂下的矩阵相乘
+/*
+#include <iostream>
+#include <cstdio>
+
+using namespace  std;
+
+void Multiply(int resul[10][10],int a[10][10],int n) 
+{
+	int c[10][10];//用来暂时保存结果
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) c[i][j] = 0;
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+
+		for (int j = 0; j < n; j++)
+		{
+
+			for (int k = 0; k < n; k++)
+			{
+				c[i][j] += resul[i][k] * a[k][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) resul[i][j] = c[i][j];
+	}
+
+}
+
+
+
+	
+
+
+void Print(int  x[10][10],int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			printf_s("%d ", x[i][j]);
+		}
+		putchar('\n');
+	}
+}
+
+int main() 
+{
+	//单位矩阵
+	
+	int reslut[10][10] = { 0 };
+	int n, k;
+	int init[10][10] = { 0 };
+	
+	while (scanf_s("%d%d",&n,&k)!=EOF)
+	{
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				scanf_s("%d", &init[i][j]);
+				//reslut[i][j] = init[i][j];
+			}
+		}
+
+		for (int i = 0; i < 10; i++){
+			for (int j = 0; j < 10; j++){
+				if (i==j){
+					reslut[i][j] = 1;
+				}
+			}
+		}
+
+		while (k!=0)
+		{
+			if (k%2==1)
+			{
+				Multiply(reslut, init,n);
+			}
+			k /= 2;
+			Multiply(init, init, n);
+		}
+
+			
+		Print(reslut,n);
+		
+
+	}
+
+	return 0;
+}
+*/
+
+//例题6 叠框问题 地址杭电oj 题号2074
+
+#include<iostream>
+#include<cstdio>
+#include<windows.h>
+using namespace std;
+
+char matrix[80][80];
+
+
+int main() 
+{
+	int n;
+	char  a, b;
+	while (scanf_s("%d%c%c",&n,&a,&b)!=EOF)
+	{
+
+		for (int i=0;i<n/2;i++)
+		{
+			int j = n - i - 1;
+			int length = n - 2 * i;
+			char c;
+			if ((n/2-i)%2==0)
+			{
+				c = a;
+			}
+			else c = b;
+			for (int k=0;k<length;k++)
+			{
+				matrix[i][i + k] = c;
+				matrix[i + k][i] = c;
+				matrix[j][j -k] = c;
+				matrix[j - k][j] = c;
+			}
+		}
+
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				printf_s("%c", matrix[i][j]);
+			}
+			putchar('\n');
+		}
+	}
+
+	
+	return 0;
+}
